6a37ec99f2b5cf6667a94cf3cc7c8256a5fd72aa
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-09-03 14:31:36 +0200

    hw 10

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,43 +1,30 @@
 import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_2ksxTUj2MSFZVGV9TXZHfNecu0yIWN4c8k2ScOV1AcrcMCLPxarX1JRERhweL1OH"
 
-
 export function fetchBreeds() {
     return fetch("https://api.thecatapi.com/v1/breeds")
     .then(response => {
-     if (!response.ok) {
-         throw new Error("Oops! Something went wrong! Try reloading the page!")
-     }
-     return response.json()
-    })
-    .then(data => {
-       return data.map(breed =>({
-        id: breed.id,
-        name: breed.name
-    }))
-})
- }
-
-export function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
-    .then(response => {
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error("Oops! Something went wrong! Try reloading the page!")
         }
         return response.json()
     })
     .then(data => {
-        if (data.length > 0){
-            const catData = data[0]
-      return {
-        name: catData.breeds[0].name,
-        description: catData.breeds[0].description,
-        temperament: catData.breeds[0].temperament,
-        image: catData.url
-        }
-    } else {
-            throw new Error("No cat data found for this breed")
-        }
+       return data.map(breed => ({
+            id : breed.id,
+            name : breed.name
+        }))
     })
+}
+
+
+export function fetchCatByBreed(breedId) {
+   return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+   .then(response => {
+    if (!response.ok) {
+        throw new Error("Oops! Something went wrong! Try reloading the page!")
+    }
+    return response.json()
+})
 }
 
--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_2ksxTUj2MSFZVGV9TXZHfNecu0yIWN4c8k2ScOV1AcrcMCLPxarX1JRERhweL1OH";
+import { Loading, Notify } from "notiflix";
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
-const errorMessage = document.querySelector('.error')
-
+const errorMessage = document.querySelector('.error');
 
 fetchBreeds().then(breeds => {
-    for (let i = 0; i < breeds.length; i++) {
-        const breed = breeds[i]
-        const options = document.createElement('option')
-        options.value = breed.id
-        options.textContent = breed.name
-        breedSelect.appendChild(options)
-    }
-})
+  breeds.forEach(element => {
+    const options = document.createElement('option');
+    options.value = element.id;
+    options.textContent = element.name;
+    breedSelect.appendChild(options);
+  });
+  new SlimSelect({
+    select: '.breed-select',
+  });
+}).catch(error => {
+  Loading.remove()
+  Notify.failure('Oops! Something went wrong! Try reloading the page!', {clickToClose: true})
+});
 
-breedSelect.addEventListener('change', handleSelectChange)
 
-function handleSelectChange() {
-const selectedBreedId = breedSelect.value
-if (selectedBreedId) {
-    loader.style.display = "block"
-    fetchCatByBreed(selectedBreedId)
-    .then(catData => {
-        const markup = `
-        <div>
-        <img src="${catData.url}" alt="${catData.name}">
-        <h1>${catData.name}</h1>
-        <p>${catData.description}</p>
-        <p>${catData.temperament}</p>
-        </div>`
-        catInfo.innerHTML = markup
-    })
-    .catch(error =>{
-        errorMessage.style.display = "block"
-    })
-    .finally(() =>{
-        loader.style.display = "none"
-    })
+breedSelect.addEventListener('change', chosenCat)
+
+
+function chosenCat(event) {
+    Loading.circle('Loading data, please wait...');
+    const breedId = event.target.value;
+    fetchCatByBreed(breedId)
 }
-}
-
